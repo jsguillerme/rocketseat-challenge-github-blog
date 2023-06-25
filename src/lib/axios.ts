616a9e8cc04c42807c2b493043a804cf09fd2f4b
Guilherme Andrade <guilhermegm.andrade@gmail.com>
2023-06-25 01:56:28 -0300

    fix: adjusts in code

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,18 +1,18 @@
 import axios from 'axios'
 
-const token = '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'
+const token = process.env.ACCESS_TOKEN
 const githubApi = 'https://api.github.com'
 
 export const API_AXIOS = axios.create({
   baseURL: githubApi,
   headers: {
-    Authorization: 'Bearer ' + token,
+    Authorization: token,
   },
 })
 
 export const API_AXIOS_ISSUES = axios.create({
   baseURL: `${githubApi}/search`,
   headers: {
-    Authorization: 'Bearer ' + token,
+    Authorization: token,
   },
 })
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
+import dotenv from 'dotenv'
 import react from '@vitejs/plugin-react'
+
+dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envDir: './.env',
+  define: {
+    ACCESS_TOKEN: `"${process.env.ACCESS_TOKEN}"`,
+  },
 })
